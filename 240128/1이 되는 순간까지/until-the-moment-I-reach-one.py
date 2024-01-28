024222a56c56b1e36886f6f9faca3587fa165bd6
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-01-28 18:50:51 +0900

    Title: 1이 되는 순간까지, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240128/1이 되는 순간까지/until-the-moment-I-reach-one.py
+++ 240128/1이 되는 순간까지/until-the-moment-I-reach-one.py
@@ -6,12 +6,13 @@ def fun(n):
     else:
         if n %2==0:
             cnt+=1
-            return fun(n//2)
+            fun(n//2)
             
         else:
             cnt+=1
-            return fun(n//3)
-            
+            fun(n//3)
+           
 N = int(input())
 cnt=0
-print(fun(N))
+fun(N)
+print(cnt)
