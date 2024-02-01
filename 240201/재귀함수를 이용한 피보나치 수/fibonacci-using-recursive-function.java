a5bbe71a7b78d426d0c851f57b3b1faa516f0979
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-02-01 17:09:40 +0900

    Title: 재귀함수를 이용한 피보나치 수, Time: 205ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240201/재귀함수를 이용한 피보나치 수/fibonacci-using-recursive-function.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int arr(int a){
+        if(a == 1){
+            return 1;
+
+        }
+        if(a == 2){
+            return 1;
+
+        }
+
+        return arr(a - 1) + arr(a - 2);
+
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System. in);
+
+        int n = sc.nextInt();
+
+        System.out.printf("%d", arr(n));
+        
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
