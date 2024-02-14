1ee80a404c3ee8459a84df4f26617d3bf6f9f37b
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-02-14 15:08:42 +0900

    Title: 국영수 순이지, Time: 168ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240214/국영수 순이지/korean-english-math-order.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class Student implements Comparable<Student> {
+    String name;
+
+    int kor, eng, math;
+
+    public Student(String name,int kor, int eng, int math){
+        this.name = name;
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+
+    }
+
+    public int compareTo(Student students){
+        if(this.kor == students.kor){
+            if(this.eng == students.eng){
+                return students.math - this.math;
+            }
+            return students.eng - this.eng ;
+
+        }
+        return students.kor - this.kor;
+
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System. in);
+
+        int n = sc.nextInt();
+
+
+        Student[] students = new Student[n];
+
+
+        for(int i = 0; i < n; i++){
+            String name = sc.next();
+
+            int kor = sc.nextInt();
+            int eng = sc.nextInt();
+            int math = sc.nextInt();
+
+
+            students[i] = new Student(name, kor, eng, math);
+
+        }
+
+        Arrays.sort(students);
+
+        for(int i = 0; i < n; i++){
+            System.out.printf("%s %d %d %d\n",students[i].name, students[i].kor, students[i].eng, students[i].math);
+
+        }
+        // 여기에 코드를 작성해주세요.
+    }
+}
