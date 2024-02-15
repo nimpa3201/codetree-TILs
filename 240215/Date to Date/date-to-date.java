933bfc53cf67c8804d181a6ce6e0c1db1326ed83
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-02-15 17:06:31 +0900

    Title: Date to Date, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240215/Date to Date/date-to-date.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System. in);
+
+
+        int[] arr = new int[]{0,31,28,31,30,31,30,31,31,30,31,30,31};
+
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        int c = 0;
+
+        while(true){
+            if(m1 == m2 && d1 == d2){
+                break;
+
+            }
+            c++;
+            d1++;
+
+            if(d1 > arr[m1]){
+                m1++;
+                d1 = 1;
+
+            }
+        }
+        c++;
+        
+        System.out.printf("%d",c);
+
+
+
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
