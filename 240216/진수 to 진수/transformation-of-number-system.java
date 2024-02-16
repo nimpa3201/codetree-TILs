776f6ab4336ea3b0ecfa6f9144ef357be3e256c8
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-02-16 18:01:27 +0900

    Title: 진수 to 진수, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240216/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System. in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        String n = sc.next();
+
+        int c = 0;
+
+        for(int i = 0; i < n.length(); i++){
+            c = c * a + (n.charAt(i) - '0');
+        }
+
+        
+        
+        int[] arr = new int[20];
+        int q = 0;
+
+        while(true){
+            if(c < 2){
+                arr[q++] = c%2;
+
+                break;
+            }
+            
+            arr[q++] = c%2;
+            c /= 2;
+
+        }
+
+
+        for(int i = q-1; i >= 0; i--){
+            System.out.printf("%d", arr[i]);
+        }           
+    
+        
+
+
+    
+        
+        // 여기에 코드를 작성해주세요.
+    }
+}
