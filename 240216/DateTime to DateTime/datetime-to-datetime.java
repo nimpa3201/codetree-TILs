8d1ae7dfa6bad3e2d500ef3fdf93742ee780eaea
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-02-16 15:19:03 +0900

    Title: DateTime to DateTime, Time: 3060ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 240216/DateTime to DateTime/datetime-to-datetime.java
+++ 240216/DateTime to DateTime/datetime-to-datetime.java
@@ -14,8 +14,6 @@ public class Main {
         int c = 0;
 
         
-        //                                1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
-        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         while(true) {
             if(day1 == day2 && hour1 == hour2 && mins1 == mins2)
                 break;
