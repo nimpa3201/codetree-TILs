f86f89384a3a02031fc1ff7628e614d8f93c4178
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-02-16 15:18:11 +0900

    Title: DateTime to DateTime, Time: 3014ms, Memory: 10MB, Status: Time Limit Exceed - CodeTree

--- 240216/DateTime to DateTime/datetime-to-datetime.java
+++ 240216/DateTime to DateTime/datetime-to-datetime.java
@@ -1,23 +1,41 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        int month = 2, day = 5;
-        int elapsedDays = 0;
+        Scanner sc = new Scanner(System. in);
+        int day1 = 11;
+        int hour1 = 11;
+        int mins1 = 11;
 
+        int day2 = sc.nextInt();
+        int hour2 = sc.nextInt();
+        int mins2 = sc.nextInt();
+        int c = 0;
+
+        
         //                                1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
         int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         while(true) {
-            if(month == 4 && day == 1)
+            if(day1 == day2 && hour1 == hour2 && mins1 == mins2)
                 break;
+            mins1++;
+            c++;
+
         
-            elapsedDays++;
-            day++;
-        
-            if(day > num_of_days[month]) {
-                month++;
-                day = 1;
+            if(mins1 == 60) {
+                hour1++;
+                mins1 = 0;
+
             }
+            if(hour1 == 24){
+                day1++;
+                hour1 = 0;
+
+            }
+
         }
         
-        System.out.print(elapsedDays);
+        System.out.println(c);
     }
 }
