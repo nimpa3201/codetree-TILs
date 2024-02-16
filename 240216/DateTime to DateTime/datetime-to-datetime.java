78386d08b176f3d0dee0ffa42b27bc5289092c39
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-02-16 15:33:36 +0900

    Title: DateTime to DateTime, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 240216/DateTime to DateTime/datetime-to-datetime.java
+++ 240216/DateTime to DateTime/datetime-to-datetime.java
@@ -4,38 +4,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System. in);
-        int day1 = 11;
-        int hour1 = 11;
-        int mins1 = 11;
 
         int day2 = sc.nextInt();
         int hour2 = sc.nextInt();
         int mins2 = sc.nextInt();
-        int c = 0;
-
         
-        while(true) {
-            if(day1 == day2 && hour1 == hour2 && mins1 == mins2){
-                break;
-            }
-            
-            mins1++;
-            c++;
+        int diff = (day2 * 24 * 60 + hour2 * 60 + mins2) - (11 * 24 * 60 + 11 * 60 + 11);
 
-        
-            if(mins1 == 60) {
-                hour1++;
-                mins1 = 0;
-
-            }
-            if(hour1 == 24){
-                day1++;
-                hour1 = 0;
-
-            }
-
+        if(diff < 0){
+            System.out.printf("%d", -1);
         }
-        
-        System.out.println(c);
+        else{
+            System.out.printf("%d", diff);
+        }
     }
 }
