6b66489358c3655ec1f9e901eebf69bc8bcd7f62
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-03-01 20:05:10 +0900

    Title: 반복 출력하기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240301/반복 출력하기/repeated-output.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arge) {
+
+        Scanner sc = new Scanner(System. in);
+
+        int a = sc.nextInt();
+
+        arr(a);
+
+
+
+
+    }
+
+    public static void arr(int a) {
+        for(int i = 0 ; i < a ; i++){
+            System.out.printf("12345^&*()_\n");
+
+        }
+    } 
+}
