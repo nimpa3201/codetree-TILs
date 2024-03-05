688a59832903fe0a6e54f597a8ff61e02870868f
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-03-05 22:35:20 +0900

    Title: 트로미노, Time: 570ms, Memory: 30MB, Status: Passed - CodeTree

--- 240305/트로미노/tromino.kt
+++ 240305/트로미노/tromino.kt
@@ -7,8 +7,8 @@ fun main(){
     }
 
     // 블럭들
-    val block1 = listOf(listOf(0, 0), listOf(0, 1), listOf(0, 2))
-    val block2 = listOf(listOf(0, 0), listOf(1, 0), listOf(2, 0))
+    val block1 = listOf(listOf(0, 0), listOf(0, 1), listOf(0, -1))
+    val block2 = listOf(listOf(0, 0), listOf(1, 0), listOf(-1, 0))
     val block3 = listOf(listOf(0, 0), listOf(1, 0), listOf(0, 1))
     val block4 = listOf(listOf(0, 0), listOf(-1, 0), listOf(0, 1))
     val block5 = listOf(listOf(0, 0), listOf(1, 0), listOf(0, -1))
@@ -19,7 +19,7 @@ fun main(){
     // 탐색
     var max = 0;
     for(r in 0 until n){
-        for(c in 0 until n){
+        for(c in 0 until m){
             // 블럭 확인
             for(block in blocks){
                 var temp = 0;
@@ -28,7 +28,7 @@ fun main(){
                     val nr = r + b[0]
                     val nc = c + b[1]
 
-                    if(nr >= 0 && nc >= 0 && nr < n && nc < n){
+                    if(nr >= 0 && nc >= 0 && nr < n && nc < m){
                         temp += arr[nr][nc]
                     }else{
                         flag = false
