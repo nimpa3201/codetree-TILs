56628dd554bac78ddd302ce39313e08efb440a60
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-03-06 16:02:44 +0900

    Title: 행복한 수열의 개수, Time: 126ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240306/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240306/행복한 수열의 개수/number-of-happy-sequence.py
@@ -8,6 +8,7 @@ for i in range(n):
     for j in range(n-1):
         if tmp == arr[i][j+1]:
             cnt1+=1
+            tmp = arr[i][j+1]
         else:
             tmp = arr[i][j+1]
     if cnt1 >=m :
@@ -20,8 +21,10 @@ for i in range(n):
     for j in range(n-1):
         if tmp == arr[j+1][i]:
             cnt2+=1
+            tmp = arr[j+1][i]
         else:
             tmp = arr[j+1][i]
+            
     if cnt2 >=m :
         ans+= 1    
     cnt2 =1
