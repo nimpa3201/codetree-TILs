309ade9cd9de7aa6b51bc3e8c575243036a56693
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-03-06 16:43:28 +0900

    Title: 행복한 수열의 개수, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240306/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240306/행복한 수열의 개수/number-of-happy-sequence.py
@@ -3,6 +3,7 @@ arr = [list(map(int,input().split()))for _ in range(n)]
 cnt1=1
 cnt2=1
 ans =0
+result=0
 for i in range(n):
     tmp = arr[i][0]
     for j in range(n-1):
@@ -11,11 +12,14 @@ for i in range(n):
            
         else:
             tmp = arr[i][j+1]
+            ans = max(ans,cnt1)
             cnt1=1
-    if cnt1 >=m :
-        ans+= 1 
-    #print(cnt1)   
+        ans = max(ans,cnt1)
+    if ans >=m :
+        result+= 1 
+    #print(ans)   
     cnt1 =1
+    ans =0
 
 
 for i in range(n):
@@ -25,10 +29,12 @@ for i in range(n):
             cnt2+=1
         else:
             tmp = arr[j+1][i]
+            ans = max(ans,cnt2)
             cnt2=1
-            
-    if cnt2 >=m :
-        ans+= 1
-    #print(cnt2)    
+        ans = max(ans,cnt2)
+    if ans >=m :
+        result+= 1
+    #print(ans)    
     cnt2 =1
-print(ans)
+    ans=0
+print(result)
