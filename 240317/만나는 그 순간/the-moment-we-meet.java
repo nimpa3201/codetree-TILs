7609356af04c752a368cca969fd2c80c8258dd3c
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-03-17 21:26:28 +0900

    Title: 만나는 그 순간, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240317/만나는 그 순간/the-moment-we-meet.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System. in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[100];
+        int[] arr2 = new int[100];
+        int c = 0;
+        int c2 = 0;
+
+
+        for(int i = 0; i < n; i++){
+            char q = sc.next().charAt(0);
+            int w = sc.nextInt();
+
+            for(int j = 1; j <= w; j++){
+                if(q == 'R'){
+                    
+                    arr[c] = c2;
+                    c2++;
+
+                }
+                else{
+                    arr[c] = c2;
+                    c2--;
+
+                }
+                c++;
+
+
+            }
+        }
+        c = 0;
+        c2 = 0;
+
+
+        
+        for(int i = 0; i < m; i++){
+            char q = sc.next().charAt(0);
+            int w = sc.nextInt();
+
+            for(int j = 1; j <= w; j++){
+                if(q == 'R'){
+                    
+                    arr2[c] = c2;
+                    c2++;
+
+                }
+                else{
+                    arr2[c] = c2;
+                    c2--;
+
+                }
+                c++;
+
+
+            }
+        }
+        int z = 1;
+
+        while(true){
+            if(arr[z] == arr2[z]){
+                break;
+
+            } 
+            z++;
+
+        }
+
+        System.out.printf("%d", z);
+
+        
+
+
+        // 여기에 코드를 작성해주세요.
+    }
+}
