f190e9f6c3f5fae7d60067bdcfd7fd9bfebac4fc
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-03-17 11:34:56 +0900

    Title: 연속되는 수 2, Time: 209ms, Memory: 13MB, Status: Passed - CodeTree

--- 240317/연속되는 수 2/continuous-number2.java
+++ 240317/연속되는 수 2/continuous-number2.java
@@ -18,27 +18,56 @@ public class Main {
 
 
         for(int i = 0; i < n; i++){
-            if( i == 0 || arr[i] == arr[i -1]){
+            if( i >= 1 && arr[i] == arr[i -1]){
                 c++;
             }
             else{
-                if(c2 < c){
-                    c2 = c;
-                    
-                }
-                c = 0;
-
+                c = 1;
             }
-        }
-        c2++;
-        if(c2 < c){
-            c2 = c;
+            c2 = Math.max(c2, c);
 
+            
         }
 
         System.out.printf("%d", c2);
-
-
         // 여기에 코드를 작성해주세요.
     }
 }
+
+/*
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX_N = 1000;
+
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // 연속하여 동일한 숫자가 나오는 횟수를 구해보며,
+        // 그 중 최댓값을 갱신합니다.
+        int ans = 0, cnt = 0;
+        for(int i = 0; i < n; i++) {
+            // Case 1
+            if(i >= 1 && arr[i] == arr[i - 1])
+                cnt++;
+            // Case 2
+            else
+                cnt = 1;
+            
+            ans = Math.max(ans, cnt);
+        }
+        
+        // 출력
+        System.out.print(ans);
+    }
+}
+*/
