4b54d31d4b31f01e51e427e7b21c3034681c3c76
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-06-27 18:38:55 +0900

    Title: 수들의 합 최대화하기, Time: 290ms, Memory: 27MB, Status: Passed - CodeTree

--- 240627/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 240627/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -1,28 +1,29 @@
 n = int(input())
-grid = [list(map(int,input().split())) for _ in range(n)]
-visited1 = [0 for _ in range(n) ]
-visited2 = [0 for _ in range(n)] 
-ans =[]
-result =0
-def maxNum(k):
-    global ans,result
-    if k == n:
-        result = max(result,sum(ans))
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+def calculate_sum(perm, grid):
+    total_sum = 0
+    for i in range(n):
+        total_sum += grid[i][perm[i]]
+    return total_sum
+
+def permutation(perm, chosen, grid, max_sum):
+    if len(perm) == n:
+        current_sum = calculate_sum(perm, grid)
+        max_sum[0] = max(max_sum[0], current_sum)
+        return
     
     for i in range(n):
-        for j in range(n):
-            if not visited1[i] and not visited2[j] and i !=j :
-                ans.append(grid[i][j])
-                visited1[i]=1
-                visited2[j]=1
-                maxNum(k+1)
-                ans.pop()
-                visited1[i]=0
-                visited2[j]=0
-    return result
-               
-if n ==1:
-    print(grid[0][0])
-else:
-                
-    print(maxNum(0))
+        if not chosen[i]:
+            perm.append(i)
+            chosen[i] = True
+            permutation(perm, chosen, grid, max_sum)
+            perm.pop()
+            chosen[i] = False
+
+max_sum = [0]
+chosen = [False] * n
+perm= []
+permutation(perm, chosen, grid, max_sum)
+
+print(max_sum[0])
