b662831d847bcc3e74dc22fc8a95dd69267d25d8
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-06-28 18:38:07 +0900

    Title: 외판원 순회, Time: 2841ms, Memory: 29MB, Status: Passed - CodeTree

--- 240628/외판원 순회/traveling-salesman-problem.py
+++ 240628/외판원 순회/traveling-salesman-problem.py
@@ -1,36 +1,33 @@
 n = int(input())
-graph = [list(map(int,input().split()))for _ in range(n)]
-visited=[0 for _ in range(n+1)]
-ans = n*10000+1
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [0 for _ in range(n)]
+ans = n * 10000 + 1
 
-def circle(arr,graph):
+def circle(arr, graph):
     global ans
-    tmp =0
-    for i in range(n):
-        if arr[i] ==i:
+    tmp = 0
+    for i in range(n - 1):
+        if graph[arr[i]][arr[i + 1]] == 0:
             return -1
-        if graph[arr[i]][i] ==0:
-            return -1
-        
-        tmp+= graph[arr[i]][i]
-    ans = min(ans,tmp)
+        tmp += graph[arr[i]][arr[i + 1]]
+    if graph[arr[-1]][arr[0]] == 0:
+        return -1
+    tmp += graph[arr[-1]][arr[0]]
+    ans = min(ans, tmp)
     return ans
 
-
-
-def perm(k,arr,n):
+def perm(k, arr, n):
     global ans
-    if k ==n and arr[-1] ==0:
-        circle(arr,graph)
-     
-    for i in range(0,n):
+    if k == n:
+        circle(arr, graph)
+        return
+    for i in range(n):
         if not visited[i]:
             arr.append(i)
-            visited[i]=1
-            perm(k+1,arr,n)
+            visited[i] = 1
+            perm(k + 1, arr, n)
             arr.pop()
-            visited[i] =0
-   
+            visited[i] = 0
 
-perm(0,[],n)
+perm(0, [], n)
 print(ans)
