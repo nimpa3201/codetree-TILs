50348992e96c7f9e692ee50122b800ab6c19e3c3
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-07-02 16:16:18 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 498ms, Memory: 28MB, Status: Passed - CodeTree

---
+++ 240702/수들 중 최솟값 최대화하기/maximin-of-numbers.py
@@ -0,0 +1,40 @@
+# 슈가코트 
+n = int(input())
+grid = [list(map(int,input().split())) for _ in range(n)]
+visited = [0 for _ in range(n)]
+max_num = 0
+def select(arr,n,sel):
+    global max_num
+    ans =[]
+    global grid
+    for i in range(n):
+        sel.append(grid[i][arr[i]])
+    sel.sort()
+    return sel[0]
+
+
+
+
+
+
+def perm(k,n,arr):
+    global max_num
+    global visited
+    if k ==n :
+        result = select(arr,n,[])
+        max_num = max(max_num,result)
+       
+    for i in range(n):
+        if not visited[i]:
+            arr.append(i)
+            visited[i]=1
+            perm(k+1,n,arr)
+            arr.pop()
+            visited[i] =0
+    
+            
+#max_num =0
+perm(0,n,[])
+print(max_num)
+#max_num = max(max_num,result)
+#print(max_num)
