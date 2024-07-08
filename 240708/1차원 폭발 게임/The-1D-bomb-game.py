83d37b4ad59b24c70010700c2b1d7ae9bc5fe1d3
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-07-08 23:51:30 +0900

    Title: 1차원 폭발 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240708/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240708/1차원 폭발 게임/The-1D-bomb-game.py
@@ -1,80 +1,54 @@
-n,m = map(int,input().split())
+n, m = map(int, input().split())
 bomb = []
 
 for _ in range(n):
     bomb.append(int(input()))
-    
-if m ==1:
-    print(0) 
-    exit()   
 
+if m == 1:
+    print(0)
+    exit()
 
-def remove(board,arr,k):
-    for r in range(len(k)):
-        for i in range(k[r][1]-arr[k[r][1]][1]+1,n):
-            if board[i] == k[r][0]:
-                board[i] =0
-    temp  = [0] * n
-    temp_idx = 0
-    for i in range(n):
-        if board[i] != 0:
-            temp[temp_idx] = board[i]
-            temp_idx+=1
-    board = temp
-    return board
+def remove(board, k):
+    for num, idx in k:
+        start_idx = idx - m + 1
+        for i in range(start_idx, idx + 1):
+            if i < len(board) and board[i] == num:
+                board[i] = 0
 
+    temp = [num for num in board if num != 0]
+    return temp
 
-
-
-def numPow(arr,k):  #터질 숫자 리턴해줌
-    global bomb
-    numIdx=[]
-    for num,cnt,idx in arr:
-        if cnt >=k: 
-            numIdx.append((num,idx))
+def numPow(arr):
+    # 연속된 숫자가 m개 이상인 숫자의 리스트 반환
+    numIdx = []
+    for i in range(len(arr)):
+        if arr[i][1] >= m:
+            numIdx.append((arr[i][0], arr[i][2]))
     return numIdx
 
-
-
-def countSequence(arr): # 연속되는 것 세는 함수
-    cnt =0
+def countSequence(arr):
+    cnt = 1
     prev = arr[0]
-    store =[]
-    for i in range(len(arr)):
-        if prev == arr[i] and prev !=0:
-            cnt+=1
-            store.append((prev,cnt,i))
+    store = []
+    for i in range(1, len(arr)):
+        if prev == arr[i] and prev != 0:
+            cnt += 1
         else:
-            cnt =1
+            store.append((prev, cnt, i - 1))
+            cnt = 1
             prev = arr[i]
-            store.append((prev,cnt,i))
-            
+    store.append((prev, cnt, len(arr) - 1))  
     return store
 
-
-# arr1=countSequence(bomb)
-# key =numPow(arr1,m) 
-# result =remove(bomb,arr1,key)
-# bomb = result
-# print(bomb)
-
-
-
 while True:
-
-    arr1=countSequence(bomb)
-    key =numPow(arr1,m) #처음 k 이상인 숫자와 인덱스 반환
-    if key ==[]:
+    arr1 = countSequence(bomb)
+    key = numPow(arr1)  
+    if not key:
         break
-    result =remove(bomb,arr1,key)
-    bomb = result
+    bomb = remove(bomb, key)
 
-ans =0
-for i  in bomb:
-    if i !=0:
-        ans+=1
+ans = len(bomb)
 
 print(ans)
-
-for i in range(ans):
-    print(bomb[i])
+for num in bomb:
+    print(num)
