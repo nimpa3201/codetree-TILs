e201087abcf75b6571b0ec60c9c82202130f7848
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-07-09 22:07:58 +0900

    Title: 1차원 폭발 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240709/1차원 폭발 게임/The-1D-bomb-game.java
+++ 240709/1차원 폭발 게임/The-1D-bomb-game.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -10,49 +9,50 @@ public class Main {
         int N = input[0];
         int M = input[1];
 
-        List<Integer> list = new ArrayList();
-        for(int i = 0; i < N; i++){
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
             list.add(Integer.parseInt(br.readLine()));
         }
-        while(true){
-            // 전체 스캔
-            ArrayList<int[]> deleteList = new ArrayList();
-            // 각 숫자의 개수를 셀 값
-            int cnt = 0;
-            // 값이 달라졌음을 비교할 이전 값
-            int prev = -1;
-            for(int i = 0; i < list.size(); i++){
-                // 이전 값과 다른 새로운 값이 들어오면 비교
-                if(prev != list.get(i)){
-                    // 이전 숫자의 개수가 M보다 크면 deleteList 넣음
-                    if(cnt >= M){
-                        deleteList.add(new int[]{prev, cnt, i - cnt});
-                    }
 
-                    cnt = 1;
-                    prev = list.get(i);
-                }else{
+        boolean changesMade = true;
+        while (changesMade) {
+            changesMade = false;
+            int cnt = 1;
+            Iterator<Integer> it = list.iterator();
+            int prev = it.next();
+            ListIterator<Integer> delStart = list.listIterator();
+
+            while (it.hasNext()) {
+                int current = it.next();
+                if (current == prev) {
                     cnt++;
+                } else {
+                    if (cnt >= M) {
+                        changesMade = true;
+                        for (int i = 0; i < cnt; i++) {
+                            delStart.next();
+                            delStart.remove();
+                        }
+                    } else {
+                        for (int i = 0; i < cnt; i++) {
+                            delStart.next();
+                        }
+                    }
+                    cnt = 1;
+                    prev = current;
                 }
             }
-            if(cnt >= M){
-                deleteList.add(new int[]{prev, cnt, list.size() - cnt});
-            }
 
-            if(deleteList.size() == 0){
-                break;
-            }
-
-            // 삭제
-            for(int[] delete : deleteList){
-                for(int i = delete[2]; i < delete[2] + delete[1]; i++){
-                    list.set(i, 0);
+            if (cnt >= M) {
+                changesMade = true;
+                for (int i = 0; i < cnt; i++) {
+                    delStart.next();
+                    delStart.remove();
                 }
             }
-
-            list = list.stream().filter(e -> e != 0).collect(Collectors.toList());
         }
+
         System.out.println(list.size());
-        list.stream().forEach(System.out::println);
+        list.forEach(System.out::println);
     }
 }
