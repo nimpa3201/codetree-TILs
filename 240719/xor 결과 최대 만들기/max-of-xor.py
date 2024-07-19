fafbf9a03604a2aa433bfe4f7d30377f590062c5
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-07-19 20:53:20 +0900

    Title: xor 결과 최대 만들기, Time: 451ms, Memory: 29MB, Status: Passed - CodeTree

--- 240719/xor 결과 최대 만들기/max-of-xor.py
+++ 240719/xor 결과 최대 만들기/max-of-xor.py
@@ -6,11 +6,14 @@ def comb(s,cnt,tmp,ans):
     if cnt == m:
         for i in tmp:
             ans^=i
+            
     for i in range(s, n):
         tmp.append(arr[i])
-        comb(s+1,cnt+1,tmp,ans)
+        comb(i+1,cnt+1,tmp,ans)
         tmp.pop()
     result = max(result,ans)
+
+  
 
 
 comb(0,0,[],0)
