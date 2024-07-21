2d6db51a52d6fa4b2042210fe0997ab8ce371f5c
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-07-21 10:10:37 +0900

    Title: 숫자의 개수, Time: 473ms, Memory: 33MB, Status: Passed - CodeTree

---
+++ 240721/숫자의 개수/number-of-integers.py
@@ -0,0 +1,37 @@
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+targets = []
+for _ in range(m):
+    targets.append(int(input()))
+
+
+
+def lower_bound(target):
+    left,right = 0,n-1
+    min_idx =n
+    while left <= right:
+        mid = (left+right)//2
+
+        if arr[mid] >=target:
+            min_idx = min(min_idx,mid)
+            right = mid-1
+        else:
+            left = mid +1
+    return min_idx
+
+def upper_bound(target):
+    left,right = 0,n-1
+    min_idx = n
+    while left <= right :
+        mid = (left+right)//2
+
+        if arr[mid]> target:
+            min_idx = min(min_idx,mid)
+            right = mid-1
+        else:
+            left = mid+1
+    return min_idx
+
+for i in targets:
+    ans =  upper_bound(i) - lower_bound(i)
+    print(ans)
