1f665ec219d137a87d26caa017b88da0a4019f61
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-07-23 13:55:31 +0900

    Title: 부분수열 여부 판단하기, Time: 138ms, Memory: 34MB, Status: Passed - CodeTree

--- 240723/부분수열 여부 판단하기/determine-subsequence.py
+++ 240723/부분수열 여부 판단하기/determine-subsequence.py
@@ -1,15 +1,14 @@
 n, m = map(int, input().split())
 arrA = list(map(int, input().split()))
 arrB = list(map(int, input().split()))
+R = 0 
 
-R = -1
 for L in range(m):
-    while R + 1 < n and arrB[L] != arrA[R + 1]:
+    while R < n and arrB[L] != arrA[R]:
         R += 1
-    if R + 1 < n and arrB[L] == arrA[R + 1]:
-        R += 1
-    else:
+    if R == n:
         print("No")
-        exit()
+        exit() 
+    R += 1  
 
 print("Yes")
