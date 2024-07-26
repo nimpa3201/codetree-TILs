fa7de922139e0624cc2e1b8eb35a4f95f874a2ef
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-07-26 20:49:43 +0900

    Title: 연속되는 수 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240726/연속되는 수 2/continuous-number2.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr =[]
+for _ in range(n):
+    arr.append(int(input()))
+
+ans =0
+cnt=0
+for i in range(n-1):
+    if arr[i] == arr[i+1]:
+        cnt+=1
+    else:
+        ans = max(ans,cnt)
+        cnt =1
+print(ans)
