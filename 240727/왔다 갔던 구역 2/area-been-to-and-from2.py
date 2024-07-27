1b63509326db12f54cb4e8ab6b48e2923275ff8d
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-07-27 15:10:22 +0900

    Title: 왔다 갔던 구역 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240727/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+cnt =0
+arr = [0 for _ in range(21)]
+start =10
+for _ in range(n):
+    step,direct = input().split()
+    step = int(step)
+    
+    if direct =="R":
+        for i in range(start+1,start+step+1):
+            arr[i] +=1
+        start = start+step+1
+
+    if direct == "L":
+        for i in range(start-1,start-step-1,-1):
+            arr[i]+=1
+        start = start-step-1
+for i in range(len(arr)):
+    if arr[i] >=2:
+        cnt+=1
+        
+        
+print(cnt)
