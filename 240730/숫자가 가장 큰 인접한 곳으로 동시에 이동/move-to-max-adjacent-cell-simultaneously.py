bcc797f313afdd0411d0e887b8ffe7c87a817d6e
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-07-30 22:09:29 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 170ms, Memory: 26MB, Status: Passed - CodeTree

--- 240730/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240730/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -1,28 +1,34 @@
 n,m,t = map(int,input().split())
 arr = [list(map(int,input().split())) for _ in range(n)]
 count = [[ 0 for _ in range(n)] for _ in range(n) ] 
-nextcount = [[ 0 for _ in range(n)] for _ in range(n) ] 
-for _ in range(m):
-    r,c = map(int,input().split())
-    count[r-1][c-1] =1
+nextcount = [[ 0 for _ in range(n)] for _ in range(n) ]
 
 
 def move():
     global nextcount 
+    # 상하좌우
     dirs = { 0 :(-1,0) , 1 :(1,0), 2:(0,-1), 3:(0,1)}
+
+    # 전체를 탐색하면서
     for i in range(n):
         for j in range(n):
-            if count[i][j] ==1:
-                x,y = i,j
-                max_val =0             
+            # 만약 구슬이 있다면
+            if count[i][j] == 1:
+                x, y = i, j
+                max_val =0
+
+                # 네 방향 탐색
                 for k in range(4):
                     nx = x + dirs[k][0]
                     ny = y + dirs[k][1]
+
+                    # 범위 내부라면
                     if 0<=nx<n and 0<=ny<n:
+
                         if max_val < arr[nx][ny]:
                             max_val = arr[nx][ny]
                             pos_x,pox_y = nx,ny
-                nextcount[pos_x][pox_y] =1
+                nextcount[pos_x][pox_y] += 1
     copy()
     nextcount = [[ 0 for _ in range(n)] for _ in range(n) ] 
     
@@ -38,19 +44,21 @@ def remove():
         for j in range(n):
             if count[i][j] >=2:
                 count[i][j] =0
-    
-            
-
+          
 def simulation():
- 
     move()
     remove()
 
+for _ in range(m):
+    r,c = map(int,input().split())
+    count[r-1][c-1] = 1
 
 for _ in range(t):
     simulation()
-ans =0
+
+ans = 0
 for i in range(n):
     for j in range(n):
         ans +=count[i][j]
+        
 print(ans)
