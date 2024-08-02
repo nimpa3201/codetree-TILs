8475a7b0ef539b1c74349c9eef773d3f461e8190
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-08-03 07:30:44 +0900

    Title: 최장 공통 부분 수열 2, Time: 279ms, Memory: 31MB, Status: Passed - CodeTree

--- 240803/최장 공통 부분 수열 2/longest-common-sequence-2.py
+++ 240803/최장 공통 부분 수열 2/longest-common-sequence-2.py
@@ -10,9 +10,8 @@ for i in range(1,len_a+1):
     for j in range(1,len_b+1):
         if A[i-1] == B[j-1]:
             dp[i][j] = dp[i-1][j-1]+1
-        else: 
-            if dp[i-1][j] >= dp[i][j-1]:
-                dp[i][j] = max(dp[i-1][j],dp[i][j-1])
+        else:
+            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
         
 def find_text(r,c):
     if r ==0 or c ==0:
