bc5e310aad3de5cd2d9d1248781addcb13c21035
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-08-02 12:51:23 +0900

    Title: 최대 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240802/최대 동전 거슬러주기/max-coin-change.py
+++ 240802/최대 동전 거슬러주기/max-coin-change.py
@@ -2,13 +2,13 @@ import sys
 n,m = map(int,input().split())
 coin = list(map(int,input().split()))
 INT_MIN = - sys.maxsize
-dp = [ INT_MIN for _ in range(m)]
+dp = [ INT_MIN for _ in range(m+1)]
 dp[0] =0
 
-for i in range(1,m):
+for i in range(1,m+1):
     for j in range(n):
         if dp[i-coin[j]] == INT_MIN:
             continue
         if i >= coin[j]:
             dp[i] = max(dp[i],dp[i-coin[j]]+1)
-print(dp[m-1] if dp != INT_MIN else -1)
+print(dp[m] if dp[m] != INT_MIN else -1)
