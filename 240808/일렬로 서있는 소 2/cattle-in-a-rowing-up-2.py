e1e96d45b67f92525d618c558936984b3dd68727
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-08-08 20:56:05 +0900

    Title: 일렬로 서있는 소 2, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240808/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ 240808/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -1,9 +1,30 @@
+# 백트랙킹
 n = int(input())
-cows = list(map(int,input().split()))
-ans =0
-for i in range(n):
-    for j in range(i+1,n):
-        for k in range(j+1,n):
-            if cows[i]<= cows[j]<=cows[k]:
-                ans+=1
-print(ans)
+arr = list(map(int,input().split()))
+visited = [ 0 for _ in range(n)]
+ans =[]
+result =0
+def permutation(start,num):
+    global result
+    max_num =0
+    cnt =0
+    if num == 3:
+        for i in ans:
+            if i >= max_num:
+                max_num = i
+                cnt+=1
+        if cnt== 3:
+            result+=1
+        return
+
+    for i in range(start,n):
+        if not visited[i]:
+            ans.append(arr[i])
+            visited[i] =1
+            permutation(i+1,num+1)
+            ans.pop()
+            visited[i]=0
+    
+permutation(0,0)
+
+print(result)
