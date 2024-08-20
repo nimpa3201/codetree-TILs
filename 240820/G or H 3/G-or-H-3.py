4fb106f1942f0b46ac29c4ff85592848940ed25f
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-08-20 22:11:40 +0900

    Title: G or H 3, Time: 199ms, Memory: 25MB, Status: Passed - CodeTree

--- 240820/G or H 3/G-or-H-3.py
+++ 240820/G or H 3/G-or-H-3.py
@@ -1,25 +1,30 @@
-n,k = map(int,input().split()) #n은 사람수 k 는 사진크기 
-max_dot=0
-arr =[]
-for _ in range(n):
+n, k = map(int, input().split())  # n은 사람 수, k는 사진 크기
+max_dot = 0
+arr = []
 
-    dot,alph = input().split()
+for _ in range(n):
+    dot, alph = input().split()
     dot = int(dot)
     if dot > max_dot:
         max_dot = dot
-    arr.append((dot,alph))
+    arr.append((dot, alph))
 
-
-photo = [ 0 for _ in range(max_dot+1)]
-for dot,alph in arr :
-    if alph == "G" :
-        photo[dot] =1
+photo = [0 for _ in range(max_dot + 1)]
+for dot, alph in arr:
+    if alph == "G":
+        photo[dot] = 1
     else:
-        photo[dot] =2
-ans =0
-for i in range(max_dot-k+1):
-    tmp=0
-    for j in range(i,i+k+1):
-        tmp+=photo[j]
-    ans = max(ans,tmp)
+        photo[dot] = 2
+
+
+if k > max_dot:
+    ans = sum(photo)
+else:
+    ans = 0
+    for i in range(max_dot - k + 1):
+        tmp = 0
+        for j in range(i, i + k+1):
+            tmp += photo[j]
+        ans = max(ans, tmp)
+
 print(ans)
