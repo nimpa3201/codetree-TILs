3e26fc099803198fc69cb6318c13e9bda554afe6
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-08-24 14:23:33 +0900

    Title: n개의 점 중 m개 고르기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240824/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -0,0 +1,28 @@
+n,m = map(int,input().split())
+dots = []
+result=0
+ans =0
+result1=201
+for _ in range(n):
+    x,y = map(int,input().split())
+    dots.append((x,y))
+
+
+
+def select(start,cnt,arr,tmp):
+    global result,ans,result1
+    if cnt == m :
+        result =0
+        for i in range(len(tmp)):
+            for j in range(i+1,len(tmp)):
+                ans = (tmp[i][0]-tmp[j][0])**2 + (tmp[i][1] -tmp[j][1])**2
+                result = max(result,ans)
+        result1 =min(result1,result)      
+
+    for i in range(start,len(arr)):
+        tmp.append(arr[i])
+        select(i+1,cnt+1,arr,tmp)
+        tmp.pop()
+
+select(0,0,dots,[])
+print(result1)
