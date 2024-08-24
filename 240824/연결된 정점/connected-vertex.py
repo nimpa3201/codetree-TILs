de4aee9b6cab566c60f8fa364ca7bcd4e5662161
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-08-24 12:47:13 +0900

    Title: 연결된 정점, Time: 811ms, Memory: 29MB, Status: Wrong Answer - CodeTree

--- 240824/연결된 정점/connected-vertex.py
+++ 240824/연결된 정점/connected-vertex.py
@@ -1,5 +1,6 @@
 n,m = map(int,input().split())
-uf =[0]+[ i for i in range(1,n+1)]
+uf =[ i for i in range(n+1)]
+size =[1] * (n+1)
 
 def find(x):
     if uf[x] ==x:
@@ -7,17 +8,21 @@ def find(x):
     uf[x] = find(uf[x])
     return uf[x]
 
-def union(X,Y):
-    X,Y = find(X),find(Y)
-    uf[X] = find(Y)
+def union(x,y):
+    X,Y = find(x),find(y)
+    
+    if X != Y:
+        if size[X] < size[Y]:
+            X,Y = Y,X
+        uf[Y] = X
+    size[X]+=size[Y]
+
+
+
 
 for _ in range(m):
     arr =list(input().split())
     if arr[0] == "x":
         union(int(arr[1]),int(arr[2]))
     else:
-        cnt =0
-        for i in uf:
-            if i == uf[int(arr[1])] :
-                cnt+=1
-        print(cnt)
+        print(size[int(arr[1])])
