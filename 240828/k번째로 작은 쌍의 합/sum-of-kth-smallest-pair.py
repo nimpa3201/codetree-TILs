e9d1babac08feac50c49e6d881d64238f33e6666
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-08-28 11:42:15 +0900

    Title: k번째로 작은 쌍의 합, Time: 5026ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 240828/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
+++ 240828/k번째로 작은 쌍의 합/sum-of-kth-smallest-pair.py
@@ -1,15 +1,20 @@
 import heapq
-n,m,k = map(int,input().split())
-arr1 = list(map(int,input().split()))
-arr2 = list(map(int,input().split()))
-pq =[]
+
+n,m,k = map(int, input().split())
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+pq = []
 
 for i in range(n):
     num1 = arr1[i]
     for j in range(m):
-        num2 = arr2[j]   
-        heapq.heappush(pq,num1+num2)
-
-for i in range(k):
-   ans = heapq.heappop(pq)
+        num2 = arr2[j]
+        sum_val = num1 + num2
+        
+        if len(pq) < k:
+            heapq.heappush(pq, -sum_val)
+        else:
+            if sum_val < -pq[0]:
+                heapq.heapreplace(pq, -sum_val)
+ans = -heapq.heappop(pq)
 print(ans)
