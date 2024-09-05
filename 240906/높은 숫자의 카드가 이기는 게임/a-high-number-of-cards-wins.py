54979388c7da92698b3d9240e2034d805ef23d6e
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-09-06 06:16:49 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 466ms, Memory: 30MB, Status: Passed - CodeTree

--- 240906/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240906/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -1,26 +1,23 @@
 import heapq
+
 n = int(input())
 Bcard = [int(input()) for _ in range(n)]
 
-Acard = list()
-for i in range(1,2*n+1):
-    if i not in Bcard:
-        Acard.append(i)
-Apq =[]
-Bpq =[]
-for i in range(n):
-        heapq.heappush(Apq,Acard[i])
-        heapq.heappush(Bpq,Bcard[i])
+Bcard_set = set(Bcard)
+Acard = [i for i in range(1, 2 * n + 1) if i not in Bcard_set]
 
-Belem= heapq.heappop(Bpq)
-cnt =0
-while True :
-        Aelem = heapq.heappop(Apq)
-        if Belem< Aelem:
-                cnt +=1
-                if len(Bpq) >0 and len(Apq)>0:
-                        Belem = heapq.heappop(Bpq)
-                else:
-                        break
+heapq.heapify(Acard)
+heapq.heapify(Bcard)
+
+cnt = 0
+while Bcard:
+    Belem = heapq.heappop(Bcard)
+    
+    while Acard and Acard[0] < Belem:
+        heapq.heappop(Acard)
+    
+    if Acard:
+        heapq.heappop(Acard)
+        cnt += 1
 
 print(cnt)
