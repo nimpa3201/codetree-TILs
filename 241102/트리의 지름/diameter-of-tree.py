505ba7203e3c349d2c664f5103b3624808434f20
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-11-02 16:24:02 +0900

    Title: 트리의 지름, Time: 258ms, Memory: 29MB, Status: Wrong Answer - CodeTree

---
+++ 241102/트리의 지름/diameter-of-tree.py
@@ -0,0 +1,30 @@
+n = int(input())
+tree = [[] for _ in range(n+1)]
+visited = [ 0 for _ in range(n+1)]
+for _ in range(n-1):
+    a,b,w = map(int,input().split())
+    tree[a].append((b,w))
+    tree[b].append((a,w))
+
+
+ans =0
+idx =0
+def dfs(parent,acc):
+    global ans, idx
+    for child ,weight in tree[parent]:
+        if not visited[child]:
+            visited[child]=1
+            dfs(child,acc+weight)
+            visited[child] =0
+    
+    if ans < acc:
+        ans =acc
+        idx = child
+    return idx ,ans
+
+
+visited[1] =1
+start,ans = dfs(1,0)
+visited = [ 0 for _ in range(n+1)]
+end = dfs(start,0)
+print(end[0])
