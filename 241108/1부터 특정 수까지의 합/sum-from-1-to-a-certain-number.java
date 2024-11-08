a0f9100cc2b333e360c6319dac34f2494b30e9e3
nimpa3201 <43628698+nimpa3201@users.noreply.github.com>
2024-11-08 11:57:27 +0900

    Title: 1부터 특정 수까지의 합, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241108/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.java
+++ 241108/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static int add(int n){
-        int sum =0;
+        int sum = 0;
 
         for(int i = 1; i <= n; i++){
             sum += i;
